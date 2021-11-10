4364b0197571e82520c50de62037997be7b3a0e7
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2021-11-10 11:02:16 +0000

    update example for opensafely clinic

--- analysis/report.R
+++ analysis/report.R
@@ -1,12 +1,33 @@
 library(tidyverse)
+library(glue)
 
+## import command-line arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if(length(args)==0){
+ # use for interactive testing
+ bw <- 5
+ bar_fill <- "blue"
+} else {
+ bw <- as.numeric(args[[1]])
+ bar_fill <- as.character(args[[2]])
+}
+
+## import input data
 df_input <- read_csv(
     here::here("output", "input.csv"),
-    col_types = cols(patient_id = col_integer(), age = col_double())
+    col_types = cols(
+        patient_id = col_integer(), 
+        age = col_double()
+        )
 )
 
+## plot histogram
 plot_age <- df_input %>%
     ggplot(aes(age)) +
-    geom_histogram()
+    geom_histogram(binwidth = bw, fill = bar_fill)
 
-ggsave(plot = plot_age, filename="descriptive_gg.png", path=here::here("output"))
+## save histogram
+ggsave(plot = plot_age,
+       filename=glue("age_hist_{bw}_{bar_fill}.png"),
+       path=here::here("output"))
